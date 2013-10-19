3eb00d30a8b38e391025244231ccb54991fc4035
Florian Gilcher <florian.gilcher@asquera.de>
2013-10-19 13:01:13 +0200

    Add validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,12 +1,20 @@
 class Entry
   include Virtus.model
   include Concerns::Indexable
+  include ActiveModel::Validations
 
   attribute :transcription, String
   attribute :tags,          Array[String]
   attribute :flags,         Array[String]
-  attribute :reviewed,      Boolean
+  attribute :reviewed,      Boolean, :default => false
   attribute :language,      String
   attribute :dialect,       String
   attribute :video,         Video
+
+  validates_presence_of :transcription,
+                        :tags,
+                        :flags,
+                        :reviewed,
+                        :language#,
+                        #:video
 end
