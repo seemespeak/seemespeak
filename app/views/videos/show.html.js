10467f905987dd29bccd6e3f45291c35ff9a6da3
jan nietfeld <hippopotame@gmail.com>
2013-10-19 19:29:56 -0300

    fixes modal view in viedeo/show.

--- app/views/videos/show.html.js
+++ app/views/videos/show.html.js
@@ -1,2 +1,5 @@
 $('#current_video').html("<%= escape_javascript( render :partial => 'entry', :locals => {:entry => @entry} ) %>");
 $('#video_popup').modal();
+var pop = Popcorn("#the_video");
+pop.loop(true);
+pop.play();
