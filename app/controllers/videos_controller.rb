6d05a5fa3c57140c595a4b3370f865a22dccae38
Florian Gilcher <florian.gilcher@asquera.de>
2013-10-19 08:59:54 +0200

    Initial import

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,16 @@
+class VideosController < ApplicationController
+  include TorqueBox::Messaging::Backgroundable
+
+  always_background :background_method
+
+  def index
+    self.class.background_method(rand(100000))
+  end
+
+  def self.background_method(int)
+    20.times do
+      sleep 1
+      Rails.logger.debug("background #{int}!!")
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seemespeak::Application.config.secret_key_base = '8f9e4214196fc65f6907cd9d6f38a1af4f35c839d2253366f7fc7df7da17891c90e94dd96d6b2f8e2e454ba932394643e4a1db144063424294039599546cfc20'
