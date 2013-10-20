af3d167c91c93faa19708a56afe24a5855e20f3b
Florian Gilcher <florian.gilcher@asquera.de>
2013-10-20 16:50:35 +0200

    Translate only if locale != language

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -1,13 +1,21 @@
 module TranslationHelper
   MAPPING = {
-    "DGS" => "de",
-    "ASL" => "en",
-    "BSL" => "en"
+    "DGS" => :de,
+    "ASL" => :en,
+    "BSL" => :en
   }
 
   # translate a word to the current locale using a sing language symbol as input
-  def sign_translate(word, sign_language)
-    translator.translate(word, from: MAPPING[sign_language], to: I18n.locale)
+  def sign_translate(entry)
+    sign_language = entry.language
+    phrase = entry.transcription
+    translate_to = MAPPING[sign_language]
+    if translate_to != I18n.locale
+      translated = translator.translate(phrase, from: MAPPING[sign_language], to: I18n.locale)
+      (phrase + " (#{translated})").html_safe
+    else
+      phrase.html_safe
+    end
   end
 
   def translator
