6bc162cb5ce92da88707f86afce40f99198a85ec
Bodo Tasche <bodo@wannawork.de>
2013-10-20 15:05:34 +0200

    Link to sign languages in wikipedia

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def link_to_language(lang)
+    link_to(content_tag("abbr", lang, {text:t("languages.#{lang.downcase}.name")}), t("languages.#{lang.downcase}.link"), )
+  end
+
 end
