38126e86c393b001d689b734fb4f1b6587f3db9b
Bodo Tasche <bodo@wannawork.de>
2014-01-04 18:20:08 +0100

    added entry to make sure the index exists

--- test/integration/admin_section_test.rb
+++ test/integration/admin_section_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class AdminSectionTest < ActionDispatch::IntegrationTest
+
+  def setup
+    begin
+      Entry.delete_all
+    rescue => e
+    end
+  end
+
   test "going to admin section should prompt http basic auth." do
   	get '/reviews'
     assert_response 401
   end
 
   test "going to admin section with valid credentials should grant access" do
+    Entry.new(:transcription => "test").index
+    sleep 1
   	user = 'admin'
     pw =   'test'
     auth = ActionController::HttpAuthentication::Basic.encode_credentials(user, pw)
