81e6f6d5753ef8d739f0f9685d8b5cf4ce44c610
Bodo Tasche <bodo@wannawork.de>
2014-01-12 18:04:23 +0100

    fixed whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Seemespeak::Application.routes.draw do
 
   resources :videos do
     member do
-  	  get 'up_vote'
+      get 'up_vote'
       get 'down_vote'
-  	end
+    end
   end
 
   resources :reviews do
     member do
-  	  get 'mark_as_moderated'
-  	end
+      get 'mark_as_moderated'
+    end
   end
 
   get "language/:locale", :controller => "locale", :action => "change"
